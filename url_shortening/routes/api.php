be8545d47d930ffb1b7cee8a5caa06d36ff53110
Agnessa Tund <agnessa.tund@tptlive.ee>
2024-04-09 10:40:41 +0300

    Authentication
    Created authentication with laravel breeze

--- url_shortening/routes/api.php
+++ url_shortening/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Passport
+Route::post('/oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
